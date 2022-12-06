ab43dc286540ef8d38698f8e107dff5b1c4b9cfb
Michael Steinle <michael.steinle@gmail.com>
2022-12-06 07:08:08 +0100

    a bit of cleanup and added the tasks

--- day6/app/Main.hs
+++ day6/app/Main.hs
@@ -1,11 +1,13 @@
 module Main where
 import Data.List (nub)
 
-prefix2 a n | nub (take n a) == take n a = n
-          | otherwise = 1 + prefix2 (tail a) n
+prefix :: Eq a => [a] -> Int -> Int
+prefix a n | nub (take n a) == take n a = n
+           | otherwise = 1 + prefix (tail a) n
 
 main :: IO ()
 main = do 
     f <- readFile "./input.txt"
-    print $ prefix2 f 14
+    print $ prefix f 4
+    print $ prefix f 14
 
