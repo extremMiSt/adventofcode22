90e1043e993b835d37970d7bc61b0ebd12bdc372
Michael Steinle <michael.steinle@gmail.com>
2022-12-09 10:28:38 +0100

    day9 done

---
+++ day9/app/Main.hs
@@ -0,0 +1,61 @@
+module Main where
+import Data.List (nub)
+
+type Position = (Integer, Integer)
+type Cmd = (Char, Integer)
+type Rope2 = [Position]
+
+toCmd :: String -> Cmd
+toCmd l = (c, d)
+    where 
+        c = head l
+        d = read $ drop 2 l
+
+
+move :: Rope2 -> [Position] -> Cmd -> (Rope2, [Position])
+move r l (_ , 0) = (r , l)
+move ((hx,hy):rs) l ('U', d) = move newPos newL ('U', d-1) 
+    where
+        newPos = moveTail ((hx,hy+1):rs)
+        newL = last newPos : l
+move ((hx,hy):rs) l ('D', d) =  move newPos newL ('D', d-1) 
+    where
+        newPos = moveTail ((hx,hy-1):rs)
+        newL = last newPos : l 
+move ((hx,hy):rs) l ('L', d) = move newPos newL ('L', d-1) 
+    where
+        newPos = moveTail ((hx-1,hy):rs)
+        newL = last newPos : l
+move ((hx,hy):rs) l ('R', d) =  move newPos newL ('R', d-1) 
+    where
+        newPos = moveTail ((hx+1,hy):rs)
+        newL = last newPos : l
+
+moveTail :: Rope2 -> Rope2 
+moveTail ((hx,hy):(tx,ty):rs)  | max (abs(hx - tx)) (abs(hy-ty)) <= 1 = (hx,hy):(tx,ty):rs 
+                                | hx == tx && hy > ty = (hx,hy) : moveTail ((tx,ty+1):rs)
+                                | hx == tx && hy < ty = (hx,hy) : moveTail ((tx,ty-1):rs)
+                                | hy == ty && hx > tx = (hx,hy) : moveTail ((tx+1,ty):rs)
+                                | hy == ty && hx < tx = (hx,hy) : moveTail ((tx-1,ty):rs)
+                                | hx > tx && hy > ty = (hx,hy) : moveTail ((tx+1,ty+1):rs)
+                                | hx < tx && hy > ty = (hx,hy) : moveTail ((tx-1,ty+1):rs)
+                                | hx < tx && hy < ty = (hx,hy) : moveTail ((tx-1,ty-1):rs)
+                                | hx > tx && hy < ty = (hx,hy) : moveTail ((tx+1,ty-1):rs)
+moveTail a = a
+
+moveAll :: Rope2 -> [Position] -> [Cmd] -> (Rope2, [Position])
+moveAll r p (c:cs) = moveAll r' p' cs
+    where 
+        (r', p') = move r p c
+moveAll r p [] = (r,p)
+
+initR :: Int -> Rope2
+initR x = replicate x (0, 0)
+
+main :: IO ()
+main = do
+    f <- readFile "./input.txt"
+    --f <- readFile "./test.txt"
+    let cmds = map toCmd (lines f)
+    print $ (length.nub.snd) (moveAll (initR 2) [last (initR 2)] cmds)
+    print $ (length.nub.snd) (moveAll (initR 10) [last (initR 10)] cmds)
