ae95dec6b59f722a554b7299801ac59d3dfa69f9
Michael Steinle <michael.steinle@gmail.com>
2022-12-21 10:49:50 +0100

    another attempt at 19... still doesn't work

--- day19/app/Main.hs
+++ day19/app/Main.hs
@@ -6,32 +6,42 @@ module Main where
 import qualified Data.Set as S
 import qualified Data.Sequence as Q
 import Data.Sequence (Seq((:<|)), (><))
+import Debug.Trace (trace)
 
 class Ord a => Graph b a where 
     neighbours :: b -> a -> [a]
 
-{-instance Graph (S.Set Cube) Cube where
-  neighbours :: S.Set Cube -> Cube -> [Cube]
-  neighbours set (a,b,c) = l1
-    where 
-        l1 = if ((a-1) >= -1) && S.notMember (a-1, b, c) set then (a-1, b, c) : l2 else l2
-        l2 = if ((a+1) <= 20) && S.notMember (a+1, b, c) set then (a+1, b, c) : l3 else l3
-        l3 = if ((b-1) >= -1) && S.notMember (a, b-1, c) set then (a, b-1, c) : l4 else l4
-        l4 = if ((b+1) <= 20) && S.notMember (a, b+1, c) set then (a, b+1, c) : l5 else l5
-        l5 = if ((c-1) >= -1) && S.notMember (a, b, c-1) set then (a, b, c-1) : l6 else l6
-        l6 = [(a, b, c + 1) | ((c + 1) <= 20) && S.notMember (a, b, c + 1) set]
--}
-
 reacheableBFS :: (Graph b a) => b -> S.Set a -> Q.Seq a -> S.Set a
-reacheableBFS graph visited Q.Empty = S.empty
+reacheableBFS graph visited Q.Empty = visited
 reacheableBFS graph visited (e:<|q) | e `elem` visited = reacheableBFS graph visited q
-                                    | otherwise = S.insert e (reacheableBFS graph (S.insert e visited) (q >< Q.fromList (neighbours graph e)))
+                                    | otherwise = reacheableBFS graph (S.insert e visited) (q >< Q.fromList (neighbours graph e))
 
 type Id = Integer
 type Ore = Integer
 type Clay = Integer
 type Obsidian = Integer
 type Blueprint = (Id, Ore, Ore, Ore, Clay, Ore, Obsidian)
+
+getId :: Blueprint -> Id
+getId (id, _,_,_,_,_,_) = id
+
+getObOre :: Blueprint -> Id
+getObOre (_,obOre,_,_,_,_,_) = obOre
+
+getCbOre :: Blueprint -> Ore
+getCbOre (_,_,cbOre,_,_,_,_) = cbOre
+
+getBbOre :: Blueprint -> Ore
+getBbOre (_,_,_,bbOre,_,_,_) = bbOre
+
+getBbClay :: Blueprint -> Clay
+getBbClay (_,_,_,_,bbClay,_,_) = bbClay
+
+getGbOre :: Blueprint -> Ore
+getGbOre (_,_,_,_,_,gbOre,_) = gbOre
+
+getGbObs :: Blueprint -> Obsidian
+getGbObs (_,_,_,_,_,_,gbObs) = gbObs
 
 parse :: String -> (Id, Ore, Ore, Ore, Clay, Ore, Obsidian)
 parse s = (read $ init (token!!1), read (token!!6), read (token!!12), 
@@ -47,23 +57,45 @@ type ObsidianRobot = Integer
 type GeodeRobot = Integer
 type State = (Time, Ore, OreRobot, Clay, ClayRobot, Obsidian, ObsidianRobot, Geode, GeodeRobot)
 
-instance Graph Blueprint State where
-  neighbours :: Blueprint -> State -> [State]
-  neighbours blue (t,o,ob,c,cb,b,bb,g,gb) = undefined
-    where 
-        t' = t+1
-        o' = o + ob
-        c' = c + cb
-        b' = b + bb
-        g' = g + gb
+getGeodes :: State -> Geode
+getGeodes (_,_,_,_,_,_,_,g,_) = g
 
+instance Graph Blueprint State where
+    neighbours :: Blueprint -> State -> [State]
+    neighbours blue s@(t,o,ob,c,cb,b,bb,g,gb) = dbg2 s (keep ++ nob ++ ncb ++ nbb ++ ngb)
+        where 
+            t' = t + 1
+            o' = o + ob
+            c' = c + cb
+            b' = b + bb
+            g' = g + gb
+            time = t<24
+            obPoss = o == getObOre blue
+            cbPoss = o >= getCbOre blue
+            --bbPoss = ((o == getBbOre blue) && (c >= getBbClay blue)) || ((o >= getBbOre blue) && (c == getBbClay blue))
+            bbPoss = (o >= getBbOre blue) && (c >= getBbClay blue)
+            --gbPoss = ((o == getGbOre blue) && (b >= getGbObs blue)) || ((o >= getGbOre blue) && (b == getGbObs blue))
+            gbPoss = (o >= getGbOre blue) && (b >= getGbObs blue)
+            keep = [(t',o',ob,c',cb,b',bb,g',gb) | time]
+            nob = [(t',o'-getObOre blue,ob+1,c',cb,b',bb,g',gb) | time && obPoss]
+            ncb = [(t',o'-getCbOre blue,ob,c',cb+1,b',bb,g',gb) | time && cbPoss]
+            nbb = [(t',o'-getBbOre blue,ob,c'-getBbClay blue,cb,b',bb+1,g',gb) | time && bbPoss]
+            ngb = [(t',o'-getGbOre blue,ob,c',cb,b'-getGbObs blue,bb,g',gb+1) | time && gbPoss]
 
 main :: IO ()
 main = do
-    f <- readFile "./input.txt"
+    --f <- readFile "./input.txt"
     f <- readFile "./test.txt"
 
-    print $ map parse (lines f)
+    let blueprints = map parse (lines f)
+    let possible = reacheableBFS (blueprints!!1) S.empty (Q.singleton (0,0,1,0,0,0,0,0,0))
+    print $ head blueprints
+    print $ maximum $ S.map getGeodes possible
 
     putStrLn "done"
 
+dbg :: Show a => a -> a
+dbg a = trace (show a ) a
+
+dbg2 :: (Show a, Show m) => m -> a -> a
+dbg2 m a = trace (show m ++ ": " ++ show a ) a
